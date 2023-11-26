fa89c85c227b55baeb2a076eeda42774598ae55d
Saleh545 <141879902+Saleh545@users.noreply.github.com>
2023-11-26 14:42:50 -0800

    .

---
+++ JS/Project.js
@@ -0,0 +1,8 @@
+document.querySelector('.select .selected').addEventListener('click', function () {
+    document.querySelector('.select').classList.add('show')
+})
+document.querySelectorAll('.dropdown a').forEach(function (item) {
+    item.addEventListener('click', function () {
+        document.querySelector('.select').classList.remove('show')
+    })
+})
